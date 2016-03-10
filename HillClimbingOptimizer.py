91453f0daf9ede44bd4839d1d974239fb9773061
camsmith293 <camsmith293@gmail.com>
2016-03-10 18:44:15 -0500

    Added output to learning loop

--- HillClimbingOptimizer.py
+++ HillClimbingOptimizer.py
@@ -5,12 +5,14 @@ class HillClimbingOptimizer():
     def __init__(self):
         self.learner = NeuralNetLearner()
         self.neural_net = self.learner.network
-        dataset = self.learner.ds
-        self.optimizer = HillClimber(dataset.evaluateModuleMSE, self.neural_net, minimize=True)
+        self.dataset = self.learner.ds
+        self.optimizer = HillClimber(self.dataset.evaluateModuleMSE, self.neural_net, minimize=True)
 
     def learn(self,n_steps):
         for i in range(n_steps):
+            print("Iteration %d:", i)
             self.neural_net = self.optimizer.learn()
+            print(self.dataset.evaluateModuleMSE(self.neural_net), "\n")
 
 h = HillClimbingOptimizer()
 h.learn(1)
