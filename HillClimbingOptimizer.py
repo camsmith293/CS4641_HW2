13e35eece24cd12e55c9b63a6f6960335c35938a
camsmith293 <camsmith293@gmail.com>
2016-03-12 12:37:54 -0500

    Implemented random restarts

--- HillClimbingOptimizer.py
+++ HillClimbingOptimizer.py
@@ -7,11 +7,25 @@ class HillClimbingOptimizer():
         self.neural_net = self.learner.network
         self.dataset = self.learner.ds
         self.training_set, self.testing_set = self.learner.get_datasets()
-        self.optimizer = HillClimber(self.training_set.evaluateModuleMSE, self.neural_net, minimize=True,
-                                     verbose = True, numParameters = 661)
 
-    def learn(self):
-        self.neural_net = self.optimizer.learn()
+        # Optimizer will take 2000 steps and restart, saving the best model from the restarts
+        self.optimizer = HillClimber(self.training_set.evaluateModuleMSE, self.neural_net, minimize=True,
+                                     verbose = True, numParameters = 661, maxLearningSteps = 2000)
+
+    def learn(self, num_restarts):
+        # Save best model and lowest MSE for random restarting
+        best_model = self.neural_net
+        min_MSE = 2147438647
+
+        temp = self.neural_net
+        for i in range(num_restarts):
+            temp = self.optimizer.learn()
+            temp_MSE = self.testing_set.evaluateModuleMSE(temp)
+            if temp_MSE <= self.testing_set.evaluateModuleMSE(best_model):
+                best_model = temp
+                min_MSE = temp_MSE
+
+        self.neural_net = best_model
 
 h = HillClimbingOptimizer()
-h.learn()
+h.learn(5)
