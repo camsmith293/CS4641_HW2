fc9750ba06f3f2057d2e414677ae9d3a6f4f4b9f
camsmith293 <camsmith293@gmail.com>
2016-03-13 01:29:05 -0500

    Solves FourPeaks

--- OptimizationProblems/FourPeaks.py
+++ OptimizationProblems/FourPeaks.py
@@ -2,11 +2,12 @@ from random import choice, randint
 from pybrain.structure.evolvables.evolvable import Evolvable
 
 class FourPeaks(Evolvable):
-    def __init__(self):
-        self.length = len(self.model)
-        self.t = int(self.length/10)
-        self.model = "" * self.length
-        self.randomize()
+    def __init__(self, bitstring):
+        self.length = len(bitstring)
+        self.t = self.length/10
+        self.model = [0] * self.length
+        for i in range(len(bitstring)):
+            self.model[i] = int(bitstring[i])
 
     def f(self):
         tail = self.tail('0')
@@ -14,10 +15,16 @@ class FourPeaks(Evolvable):
         return max(tail, head) + self.R()
 
     def tail(self, b):
-        return (len(self.model) - len(self.model.rstrip(b)))
+        stringed = ""
+        for i in self.model:
+            stringed += str(self.model[i])
+        return (len(stringed) - len(stringed.rstrip(b)))
 
     def head(self, b):
-        return len(self.model.split('0', 1)[0])
+        stringed = ""
+        for i in self.model:
+            stringed += str(self.model[i])
+        return len(stringed.split('0', 1)[0])
 
     def R(self):
         tail_bool = self.tail('0') > self.t
@@ -26,18 +33,21 @@ class FourPeaks(Evolvable):
         else: return 0
 
     def mutate(self, **args):
-        index = choice(list(range(self.length)))
-        if self.model[index] is '0':
-            self.model[index] = '1'
+        index = choice(list(range(0,len(self.model))))
+        if self.model[index] is 0:
+            self.model[index] = 1
         else:
-            self.model[index] = '0'
+            self.model[index] = 0
 
     def randomize(self):
-        model_list = [randint(0,1) for b in range(self.length)]
-        self.mode = ''.join(str(e) for e in model_list)
+        for i in range(len(self.model)):
+            self.model[i] = choice([0,1])
 
     def domain(self):
-        return [0,1] * self.length
+        return [0,1] * len(self.model)
+
+f = FourPeaks('11100100')
+print(f.f())
 
 
 
