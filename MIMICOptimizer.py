071c53e7a4918f957229efcbfe12d9fc1b7bfb53
camsmith293 <camsmith293@gmail.com>
2016-03-13 21:25:34 -0400

    Shotgun commit

--- MIMICOptimizer.py
+++ MIMICOptimizer.py
@@ -35,7 +35,7 @@ class MIMICOptimizer():
 
     def learn_optimizationproblem(self, iterations, problem, fitness_function, minimize=False):
         self.optimizer = Mimic(problem.domain(), fitness_function, samples=250,
-                maximize=True, discreteValues=True, percentile=0.9, minimize=minimize)
+                maximize=not(minimize), discreteValues=True, percentile=0.9)
 
         evaluations = []
         out_name = 'out/opt_mimic_evaluations_' + problem.__class__.__name__ + '.csv'
