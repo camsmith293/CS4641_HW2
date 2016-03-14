934c909b05da44c786eb36ce8170ffa0b441b171
camsmith293 <camsmith293@gmail.com>
2016-03-13 23:22:13 -0400

    Shotgun commit

--- OptimizationProblems/kColors.py
+++ OptimizationProblems/kColors.py
@@ -30,13 +30,19 @@ class kColors(Evolvable):
         self.neighbors['TamilNadu'] = ['Andhra', 'Karnataka', 'Kerala']
         self.neighbors['Kerala'] = ['Karnataka', 'TamilNadu']
 
-        self.model = {}
+        self.model_dict = dict.fromkeys(states)
+        self.model = [self.model_dict.get('Andhra'),
+                      self.model_dict.get('Karnataka'),
+                      self.model_dict.get('TamilNadu'),
+                      self.model_dict.get('Kerala')
+                ]
+
         self.randomize()
 
     def promising(self, state, color):
         collisions = 0
         for neighbor in self.neighbors.get(state):
-            color_of_neighbor = self.model.get(neighbor)
+            color_of_neighbor = self.model_dict.get(neighbor)
             if color_of_neighbor == color: collisions += 1
 
         return collisions
@@ -48,12 +54,12 @@ class kColors(Evolvable):
 
     def mutate(self, **args):
         state = choice(self.states)
-        exclusionary = [elem for elem in self.colors if elem != self.model[state]]
-        self.model[state] = choice(exclusionary)
+        exclusionary = [elem for elem in self.colors if elem != self.model_dict[state]]
+        self.model_dict[state] = choice(exclusionary)
 
     def randomize(self):
         for state in self.states:
-            self.model[state] = choice(self.colors)
+            self.model_dict[state] = choice(self.colors)
 
     def domain(self):
         return self.colors * len(self.states)
